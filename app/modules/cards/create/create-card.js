1f3c3e767b1062bd879e45afe9f6323190f2cfd0
Ann <annagorova66@gmail.com>
2018-12-05 21:01:32 +0200

    commit

---
+++ app/modules/cards/create/create-card.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const fs = require('fs');
+// const path = require('path');
+const app = express();
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+// module.exports = (app, db) => {
+    app.get('/notes', (req, res) => {
+            res.render('create-card');
+        }
+    );
+    // app.route('/notes')
+    //     .get((req, res) => {
+    //         res.render('create-card', {});
+    //         }
+    //     )
+    //
+    //     .post(async (req, res) => {
+    //         let notes = {
+    //             title: req.body.title,
+    //             description: req.body.title
+    //         };
+    //
+    //         let result = null;
+    //
+    //         try {
+    //             result = await db.collection('testNotes').insertOne(notes);
+    //         } catch (err) {
+    //             console.log(err)
+    //         }
+    //
+    //         res.end('ok');
+    //     })
--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,7 @@
-const mainPage = require('./app/index') 
+// const mainPage = require('./app/index');
+
+// const addNote = require('./app/modules/cards/create/create-card');
 
 module.exports = function(app, db) {
-
+    // addNote(app, db);
 }
