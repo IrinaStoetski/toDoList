137c98c7c48d6f2bf57cafe29853632339d540e6
Irina <irinastoetski13@gmail.com>
2018-12-05 21:47:22 +0200

    save

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const db = require('./config/db');
 
@@ -8,6 +9,7 @@ let app = express();
 
 app.set('views', './app/templates/');
 app.set('view engine', 'pug');
+/* app.use(express.static(__dirname + '/public')); */
 MongoClient.connect(db.url, { useNewUrlParser: true }, (err, client) => {
 	if (err) {
 		return console.log(err);
