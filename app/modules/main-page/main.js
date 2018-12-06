0089ce26dee34a305876f0dc609dffae6f2f76f7
IrinaStoetski <30476402+IrinaStoetski@users.noreply.github.com>
2018-12-07 01:51:49 +0200

    Architecture (#5)
    * architechture
    
    * pug templates
    
    * save

--- app/modules/main-page/main.js
+++ app/modules/main-page/main.js
@@ -1,9 +1,22 @@
-
+const MongoClient = require('mongodb').MongoClient;
 
 module.exports = (app, db) => {
 
-app.get('/', (req, res) => {
-	res.render('main-page', {title: 'To do list', message: 'Hi!'})
+app.get('/', async (req, res) => {
+	let result = null;
+	let notes = [];
+	try {
+		result = await db.collection('notes').find().forEach(element => {
+			arrProducts.push(element);
+		});
+		console.log(result);
+	}	
+	catch (err) {
+		console.log(err);
+	}
+	res.send(JSON.stringify(notes));
+
+	res.render('main-page', {title: 'To do list', message: 'Hi!'})	
 });
 
 }
