0832be332d8e40fc410ef82986e51ec1c4c68836
IrinaStoetski <30476402+IrinaStoetski@users.noreply.github.com>
2018-12-08 12:51:03 +0200

    bootsrap in main page (#8)

--- app/modules/main-page/main.js
+++ app/modules/main-page/main.js
@@ -6,17 +6,17 @@ app.get('/', async (req, res) => {
 	let result = null;
 	let notes = [];
 	try {
-		result = await db.collection('notes').find().forEach(element => {
-			arrProducts.push(element);
+		result = await db.collection('to-do-list').find().forEach(element => {
+			notes.push(element);
 		});
 		console.log(result);
 	}	
 	catch (err) {
 		console.log(err);
 	}
-	res.send(JSON.stringify(notes));
+/* 	res.send(JSON.stringify(notes)); */
 
-	res.render('main-page', {title: 'To do list', message: 'Hi!'})	
+	res.render('main-page', {notes: notes})	
 });
 
 }
