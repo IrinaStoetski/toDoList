d260eff52002ee8840ac1ccc499acc1476bfc88d
Ann <annagorova66@gmail.com>
2018-12-08 11:06:28 +0200

    update name db and file(create)

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,8 @@
  const mainPage = require('../app/modules/main-page/main');
 
 
-const addNote = require('../app/modules/cards/create/create-card');
-const updateNote = require('../app/modules/cards/update/update-card');
+const addNote = require('../app/modules/cards/create/addNote');
+const updateNote = require('../app/modules/cards/update/updateNote');
 
 module.exports = function(app, db) {
 	mainPage(app, db);
