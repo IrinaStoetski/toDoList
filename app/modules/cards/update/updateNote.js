621bec7621b289a3c0c4fc58a9a7aa5cfa1f2cc5
Ann <annagorova66@gmail.com>
2018-12-10 20:18:49 +0200

    update fetch

--- app/modules/cards/update/updateNote.js
+++ app/modules/cards/update/updateNote.js
@@ -1,22 +1,22 @@
-const express = require('express');
-let app = express();
+// const express = require('express');
+// let app = express();
 const ObjectId = require('mongodb').ObjectId;
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended:false}));
-
-const methodOverride = require('method-override');
-app.use(methodOverride('X-HTTP-Method'));
-app.use(methodOverride('X-HTTP-Method-Override'));
-app.use(methodOverride('X-Method-Override'));
-// app.use(methodOverride('_method', { methods: ['POST', 'PUT'] }));
-
-app.use(methodOverride(function (req, res) {
-    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-        let method = req.body._method;
-        delete req.body._method;
-        return method;
-    }
-}));
+// const bodyParser = require('body-parser');
+// app.use(bodyParser.urlencoded({extended:false}));
+//
+// const methodOverride = require('method-override');
+// app.use(methodOverride('X-HTTP-Method'));
+// app.use(methodOverride('X-HTTP-Method-Override'));
+// app.use(methodOverride('X-Method-Override'));
+// // app.use(methodOverride('_method', { methods: ['POST', 'PUT'] }));
+//
+// app.use(methodOverride(function (req, res) {
+//     if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+//         let method = req.body._method;
+//         delete req.body._method;
+//         return method;
+//     }
+// }));
 
 module.exports = (app, db) =>{
     app.get('/notes/:id', async (req, res) =>{
@@ -30,17 +30,17 @@ module.exports = (app, db) =>{
         }
         const showData = {
             id: query._id,
-            title: result.title,
+            titleNote: result.titleNote,
             description: result.description,
         };
-        res.render('updateNote', {id: showData.id, title: showData.title, description: showData.description});
-        res.send('ok');
+        res.render('updateNote', {id: showData.id, titleNote: showData.titleNote, description: showData.description});
+        // res.send('ok');
     });
 
-    app.post('/notes/:id', async (req, res)=>{
+    app.put('/notes/:id', async (req, res)=>{
         const query = {_id: ObjectId(req.params.id)};
         const newData = {
-            title: req.body.title,
+            titleNote: req.body.titleNote,
             description: req.body.description,
         };
         let result = null;
@@ -50,6 +50,7 @@ module.exports = (app, db) =>{
         } catch (err) {
             console.log(err);
         }
+        res.send(newData.titleNote + ' ' + newData.description);
         // res.send('update note');
 
         // const notes = [];
@@ -62,7 +63,7 @@ module.exports = (app, db) =>{
         //     console.log(err);
         // }
 
-        res.redirect("/");
+        // res.redirect("/");
         // res.render('main-page', {notes: notes});
     });
 };
--- public/script.js
+++ public/script.js
@@ -1,1 +1,19 @@
 console.log(45);
+
+// document.getElementById('cl').addEventListener('click', postData);
+
+function postData(event) {
+    event.preventDefault();
+
+    // let form = new FormData(document.getElementById('myform'));
+    // let firstname = document.getElementById('nameNote').value;
+    // let secondname = document.getElementById('descriptionNote').value;
+
+    const formData = new FormData(document.getElementById('myform'));
+    fetch('/notes/'+id, {method: 'put', body: formData}).then(response => {
+            response.text().then(function (titleNote, description) {
+                document.getElementById('nameNote').value = titleNote;
+                document.getElementById('descriptionNote').value = description;
+            })
+        })
+}
