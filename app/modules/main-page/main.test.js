c935e77effec973e9d04799c396af1ce01ca7784
Irina <irinastoetski13@gmail.com>
2018-12-17 15:41:07 +0200

    save

--- app/modules/main-page/main.test.js
+++ app/modules/main-page/main.test.js
@@ -1,20 +1,13 @@
-const request = require('supertest');
-const express = require('express');
-const db = require('../../../config/db');
-const MongoClient = require('mongodb').MongoClient;
-
+const request = require("supertest");
 
 describe("Main page testing", function() {
- 
-    it("Get response is 200", (done) => {		 
-			request('http://localhost:8000').get('/').expect(200).end( (err, res)=> {
+  it("Get response is 200", done => {
+    request("http://localhost:8000")
+      .get("/")
+      .expect(200)
+      .end((err, res) => {
         if (err) return done(err);
         done();
-  })
-		})
-
-/* 		it("database return objects", (done) => {
-			let array = [];
-
-});	 */
+      });
+  });
 });
