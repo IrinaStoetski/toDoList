8b6fc573eed6bf6cd90469576078a1c89702cba2
IrinaStoetski <30476402+IrinaStoetski@users.noreply.github.com>
2018-12-17 15:44:13 +0200

    Final (#22)
    * save
    
    * save

--- app/modules/notes/create/addNote.test.js
+++ app/modules/notes/create/addNote.test.js
@@ -4,15 +4,8 @@ const express = require('express');
 const app = express();
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const assert = chai.assert;
-const expect = chai.expect;
+
 chai.use(chaiHttp);
-
-let addNote = require('./addNote.js');
-
-const supertest = require("supertest");
-const server = supertest.agent("http://localhost:8000");
-const should = require('should');
 
 describe('POST /note', function() {
     it('add new note', function(done) {
@@ -26,9 +19,6 @@ describe('POST /note', function() {
             .set('Accept', 'application/json')
             .end((err, res) => {
                 if (err) return done(err);
-
-                // res.body.note.should.have.property('title');
-                // res.body.note.should.have.property('description');
                 done();
             });
     });
